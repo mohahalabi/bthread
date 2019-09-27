6e679731e98efae3215c3aad53b08e971d71f914
Mohammad Halabi <mohammad.halabi@student.supsi.ch>
2019-09-27 12:44:47 +0200

    added project directory

---
+++ bthread/headers/BThreadLib.h
@@ -0,0 +1,8 @@
+//
+// Created by Admin on 2019-09-27.
+//
+
+#ifndef BTHREAD_BTHREADLIB_H
+#define BTHREAD_BTHREADLIB_H
+
+#endif //BTHREAD_BTHREADLIB_H
---
+++ bthread/sources/BThreadLib.c
@@ -0,0 +1,5 @@
+//
+// Created by Admin on 2019-09-27.
+//
+
+#include "../headers/BThreadLib.h"
